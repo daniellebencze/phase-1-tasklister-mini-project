d6e298f4ed4d13a4897c33106392d837d5f4166a
Danielle Bencze <daniellebencze@live.com>
2021-12-20 17:27:59 -0800

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //grab all the necessary DOM elements
+
+  //form and relevant input fields
+  const newTaskForm = document.getElementById("create-task-form");
+
+  //attach event listeners
+  newTaskForm.addEventListener("submit", createNewTask);
 });
+
+const createNewTask = (event) => {
+  event.preventDefault();
+  //stop form from trying to submit
+  const newTask = document.createElement("li");
+  const newTaskDescription = document.getElementById("new-task-description");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = (task) => {
+  document.getElementById("tasks").appendChild(task);
+};
